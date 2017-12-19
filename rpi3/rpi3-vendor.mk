56df5663f6df973ad269cbc0eb393d51a4f32449
Konsta <konsta09@gmail.com>
2017-12-19 17:45:28 +0200

    rpi3: update widevine
    * From angler.
    
    Change-Id: Id44e1db21f0bd9beadb9a233bacdaf2791e68199

--- rpi3/rpi3-vendor.mk
+++ rpi3/rpi3-vendor.mk
@@ -16,8 +16,4 @@
 
 PRODUCT_COPY_FILES += \
     vendor/brcm/rpi3/proprietary/vendor/firmware/brcm/BCM43430A1.hcd:system/vendor/firmware/brcm/BCM43430A1.hcd \
-    vendor/brcm/rpi3/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-    vendor/brcm/rpi3/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-    vendor/brcm/rpi3/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-    vendor/brcm/rpi3/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
     vendor/brcm/rpi3/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
