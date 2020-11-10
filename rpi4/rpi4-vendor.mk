7327e9d402522ae6cdb976a2fdad2dbc27fec4ce
Konsta <konsta09@gmail.com>
2020-11-10 19:37:46 +0200

    rpi4: add brcm 43456 bt & wifi firmware
    * 2020-08-20-raspios-buster
    
    Change-Id: Id3d7e74490d1255617ba90e8f31d89bc113390d8

--- rpi4/rpi4-vendor.mk
+++ rpi4/rpi4-vendor.mk
@@ -18,4 +18,5 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/brcm/rpi4
 
 PRODUCT_COPY_FILES += \
-    vendor/brcm/rpi4/proprietary/vendor/firmware/brcm/BCM4345C0.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/brcm/BCM4345C0.hcd
+    vendor/brcm/rpi4/proprietary/vendor/firmware/brcm/BCM4345C0.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/brcm/BCM4345C0.hcd \
+    vendor/brcm/rpi4/proprietary/vendor/firmware/brcm/BCM4345C5.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/brcm/BCM4345C5.hcd
